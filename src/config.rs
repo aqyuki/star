e338c6d908f082db47d2b7cc9e6565e8a0abf837
aqyuki <yksk171118@gmail.com>
2024-06-09 21:16:22 +0900

    feat: add a function to setup logger

--- src/config.rs
+++ src/config.rs
@@ -3,6 +3,13 @@ pub fn load_discord_token() -> String {
         .expect("Expected a token in the environment variable DISCORD_TOKEN")
 }
 
+pub fn init_logger() {
+    if let Err(_) = std::env::var("RUST_LOG") {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init()
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
