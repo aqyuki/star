39762c3a05fab34c183d831a26957e29fc0c7468
aqyuki <yksk171118@gmail.com>
2024-06-09 15:34:24 +0900

    feat: change to start Bot in async_main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+use anyhow::{Ok, Result};
+
 mod client;
 mod config;
 
-fn main() {}
+fn main() -> Result<()> {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async_main())
+}
+
+async fn async_main() -> Result<()> {
+    let token = config::load_discord_token();
+    let client = client::discord::DiscordClient::new();
+    client.run(&token).await?;
+    Ok(())
+}
