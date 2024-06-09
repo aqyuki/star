d97ac7cb49210c4d63048a087eaf2c63f54069a5
aqyuki <117277631+aqyuki@users.noreply.github.com>
2024-06-09 15:42:36 +0900

    feat: add serenity wrapper (#3)
    * feat: add a module
    
    * chore(deps): add depended packages
    
    * feat(mod): add a module
    
    * feat: add DiscordClient structure
    
    * feat: add new function
    
    * feat: change to start Bot in async_main

---
+++ src/client/discord.rs
@@ -0,0 +1,23 @@
+use anyhow::{Context as _, Result};
+use serenity::{all::GatewayIntents, client::Client};
+
+pub struct DiscordClient;
+
+impl DiscordClient {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub async fn run(self, token: &str) -> Result<()> {
+        let mut client = Client::builder(token, GatewayIntents::default())
+            .await
+            .expect("Failed to create Discord client");
+
+        // TODO: Add event handler
+
+        client
+            .start()
+            .await
+            .context("Failed to start Discord client")
+    }
+}
