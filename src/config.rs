82d4623fddffba6489ddae186bbfca6d46123e51
aqyuki <yksk171118@gmail.com>
2024-06-09 21:40:13 +0900

    test: remove test

--- src/config.rs
+++ src/config.rs
@@ -9,21 +9,3 @@ pub fn init_logger() {
     }
     env_logger::init()
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_load_discord_token_should_success() {
-        std::env::set_var("DISCORD_TOKEN", "test_token");
-        let token = super::load_discord_token();
-        assert_eq!("test_token", token);
-        std::env::remove_var("DISCORD_TOKEN");
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_load_discord_token_should_panic() {
-        std::env::remove_var("DISCORD_TOKEN");
-        super::load_discord_token();
-    }
-}
