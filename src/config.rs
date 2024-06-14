77032c500da7ed74ccd4b18adca7b34e2dbb3cd2
aqyuki <117277631+aqyuki@users.noreply.github.com>
2024-06-15 07:35:06 +0900

    refactor: refactor code (#32)
    * refactor: refactor
    
    * test: add test

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,12 @@
+use std::env::{set_var, var};
+
 pub fn load_discord_token() -> String {
-    std::env::var("DISCORD_TOKEN")
-        .expect("Expected a token in the environment variable DISCORD_TOKEN")
+    var("DISCORD_TOKEN").expect("Expected a token in the environment variable DISCORD_TOKEN")
 }
 
 pub fn init_logger() {
-    if let Err(_) = std::env::var("RUST_LOG") {
-        std::env::set_var("RUST_LOG", "info");
+    if var("RUST_LOG").is_err() {
+        set_var("RUST_LOG", "info");
     }
     env_logger::init()
 }
---
+++ src/feature/mod.rs
@@ -0,0 +1,3 @@
+pub mod expand_link;
+pub mod health_check;
+mod model;
